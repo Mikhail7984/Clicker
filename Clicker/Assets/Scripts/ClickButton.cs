67a4409a3b90a0babc11829cc0f7b8b521187890
Mikhail <mishapomelov159@mail.ru>
2026-02-04 19:33:44 +0300

    0.0.1ver
    game is done

--- Clicker/Assets/Scripts/ClickButton.cs
+++ Clicker/Assets/Scripts/ClickButton.cs
@@ -5,12 +5,12 @@ using TMPro;
 public class ClickButton : MonoBehaviour, IPointerDownHandler
 {
     public TMP_Text clickText;
-    public int clicks;
-    public int amount = 1;
+    [SerializeField] public static int clicks;
+    public static int amount = 1;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        clicks += 1;
+        clicks += amount;
         clickText.text = $"Clicks: {clicks}";
     }
 }
--- Clicker/Assets/Scripts/Multiplier.cs
+++ Clicker/Assets/Scripts/Multiplier.cs
@@ -1,23 +1,40 @@
-//using UnityEngine;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using System.Collections;
 
-//public class Multiplier : MonoBehaviour
-//{
-//    public ClickButton click;
+public class Multiplier : MonoBehaviour, IPointerDownHandler
+{
+    public TMP_Text multiplierText;
 
-//    private void Awake()
-//    {
-//        click = GetComponent<ClickButton>();
-//    }
+    private ClickButton text;
 
-//    private void Update()
-//    {
-//        if (click.clicks >= 100)
-//        {
-//            if (Input.GetMouseButtonDown(0))
-//            {
-//                Debug.Log("multiplier is active");
-//                click.clicks += click.sum * 2;
-//            }
-//        }
-//    }
-//}
+    private void Awake()
+    {
+        text = FindAnyObjectByType<ClickButton>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (ClickButton.clicks >= 100)
+        {
+            ClickButton.clicks = 0;
+            ClickButton.amount = 2;
+            text.clickText.text = "Clicks: 0";
+            multiplierText.text = "Multiplier is bought.";
+            StartCoroutine(Wait());
+        }
+        else
+        {
+            multiplierText.text = $"Not enough clicks ({ClickButton.clicks} / 100)";
+            StartCoroutine(Wait());
+        }
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+
+        multiplierText.text = "Buy multiplier";
+    }
+}
